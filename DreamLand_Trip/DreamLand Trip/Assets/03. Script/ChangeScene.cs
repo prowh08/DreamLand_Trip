6de226797efe344d59bbbd33dc6d6f9979bb4f19
prowh08 <pro_wh08@naver.com>
2021-10-05 11:03:01 +0900

    [02] UI와 화면 크기 고정
    시작, 로딩, 맵 UI 및 화면 크기 고정

---
+++ DreamLand_Trip/DreamLand Trip/Assets/03. Script/ChangeScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void change_MapScene()
+    {
+        SceneManager.LoadScene("02. Map");
+    }
+
+    public void change_MainScene()
+    {
+        SceneManager.LoadScene("03. Main");
+    }
+}
